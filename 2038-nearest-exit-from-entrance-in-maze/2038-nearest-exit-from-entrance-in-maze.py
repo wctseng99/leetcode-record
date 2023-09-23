0c109ada4740f6fc8f3623359d57a46da8f25a4a
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-09-23 11:55:38 +0800

    Time: 764 ms (60.08%), Space: 16.8 MB (92.04%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -1,7 +1,6 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         
-    
         queue = deque([(entrance[0], entrance[1], 0)])
         maze[entrance[0]][entrance[1]] = "+"
 
@@ -9,7 +8,7 @@ class Solution:
             i, j, step = queue.popleft()
             for row, col in (i+1, j), (i-1, j), (i, j+1), (i, j-1):
                 if 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] == ".":
-                    if row == 0 or col ==0 or row == len(maze) -1 or col == len(maze[0]) - 1:
+                    if row == 0 or row == len(maze) - 1 or col == 0 or col == len(maze[0]) -1:
                         return step + 1
                     maze[row][col] = "+"
                     queue.append((row, col, step+1))
