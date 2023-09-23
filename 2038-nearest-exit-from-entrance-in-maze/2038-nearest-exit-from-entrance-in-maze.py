4b283d341fe1832bcedaedf8de4054b775109db9
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-09-23 11:42:06 +0800

    Time: 750 ms (71.65%), Space: 16.7 MB (98.58%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.py
@@ -1,22 +1,16 @@
 class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
-        queue = deque([tuple(entrance)])
-        step = 0
-        visited = set()
-        visited.add(tuple(entrance))
+        
+    
+        queue = deque([(entrance[0], entrance[1], 0)])
+        maze[entrance[0]][entrance[1]] = "+"
 
         while queue:
-            for _ in range(len(queue)):
-                i, j = queue.popleft()
-                # check if the cell a exist
-                if i == 0 or j == 0 or i == len(maze) - 1 or  j == len(maze[0]) - 1:
-                    if maze[i][j] == '.' and [i, j] != entrance:
-                        return step
-                # bfs
-                for row, col in (i+1, j), (i-1, j), (i, j+1), (i, j-1):
-                    if 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] == "." and (row, col) not in visited:
-                        visited.add((row, col))
-                        queue.append((row, col))
-                
-            step += 1
+            i, j, step = queue.popleft()
+            for row, col in (i+1, j), (i-1, j), (i, j+1), (i, j-1):
+                if 0 <= row < len(maze) and 0 <= col < len(maze[0]) and maze[row][col] == ".":
+                    if row == 0 or col ==0 or row == len(maze) -1 or col == len(maze[0]) - 1:
+                        return step + 1
+                    maze[row][col] = "+"
+                    queue.append((row, col, step+1))
         return -1
