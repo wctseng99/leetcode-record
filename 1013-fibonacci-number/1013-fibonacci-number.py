a368102dbbf75d16f3cea96271372a6415e6c36c
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-09-26 13:41:04 +0800

    Time: 42 ms (50.11%), Space: 16.3 MB (55.5%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,14 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
 
-        def fibonacci(n, dp_array):
+        def fibonacci(n):
             if dp_array[n] == -1:
                 if n <= 1:
                     dp_array[n] = n
                 else:
-                    dp_array[n] = fibonacci(n-2, dp_array) + fibonacci(n-1, dp_array)
+                    dp_array[n] = fibonacci(n-2) + fibonacci(n-1)
             return dp_array[n]
 
         dp_array = [-1] * (n+1)
-        return fibonacci(n, dp_array)
+        return fibonacci(n)
         
