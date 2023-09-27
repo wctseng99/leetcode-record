a36d959cf469493a7df0dff3f46c81a7dd639116
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-09-27 11:24:37 +0800

    Time: 416 ms (60.74%), Space: 18.7 MB (51.91%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,32 +1,25 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = defaultdict(list)
+        distances = {node: float('inf') for node in range(1, n+1)}
+        distances[k] = 0
+        visited = set()
+
         for u, v, w in times:
             graph[u].append((v, w))
-        
-        visited = set()
-        distances = {node: float('inf') for node in range(1, n+1)}
-        
-        distances[k] = 0
 
-        while len(visited) != n:
-            # Find the node with the smallest distance
-            min_dis = float('inf')
+        while len(visited) < n:
             min_node = None
-            for node in range(1, n + 1):
-                if node not in visited and distances[node] < min_dis:
-                    min_dis = distances[node]
+            min_dist = float('inf')
+            # find the node with the minimum distance
+            for node in range(1, n+1):
+                if node not in visited and distances[node] < min_dist:
                     min_node = node
-
+                    min_dist = distances[node]
             if min_node is None:
                 break
-            # Mark the node as visited
             visited.add(min_node)
-
-            # Update distances to neighboring nodes
-            for neighbor, time in graph[min_node]:
-                distance = distances[min_node] + time
-                if distance < distances[neighbor]:
-                    distances[neighbor] = distance
-
+            # update the distances
+            for v, w in graph[min_node]:
+                distances[v] = min(distances[v], distances[min_node] + w)
         return max(distances.values()) if len(visited) == n else -1
