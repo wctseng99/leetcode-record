7802c1cbf8961ef03dd6065b59e93a0af2214542
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-10-05 11:20:52 +0800

    Time: 104 ms (89.27%), Space: 18.6 MB (33.45%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,14 +1,25 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []
-        area = 0
+        peak_index = 0
+        for i in range(len(height)):
+            if height[i] > height[peak_index]:
+                peak_index = i
+        
+        left_most_bar = 0
+        water = 0
 
-        for i, h in enumerate(height):
-            while stack and stack[-1][1] < h:
-                lower_index, lower_height = stack.pop()
-                if stack:
-                    distance = i - stack[-1][0] - 1
-                    trapped_height = min(stack[-1][1], h) - lower_height
-                    area += distance * trapped_height
-            stack.append([i, h])
-        return area
+        for i in range(peak_index):
+            if height[i] > height[left_most_bar]:
+                left_most_bar = i
+            else:
+                water += height[left_most_bar] - height[i]
+
+        right_most_bar = len(height) - 1
+
+        for i in range(len(height)-1, peak_index, -1):
+            if height[i] > height[right_most_bar]:
+                right_most_bar = i
+            else:
+                water += height[right_most_bar] - height[i]
+                
+        return water
