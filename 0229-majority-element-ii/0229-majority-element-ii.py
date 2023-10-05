bb56882733444f548c38aa351c7970ebfde44eb3
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-10-05 13:22:54 +0800

    Time: 117 ms (28.55%), Space: 17.7 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,11 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        freq = defaultdict(int)
-        output = set()
+        threshold = len(nums)/3
+        freq = Counter(nums)
+        output = []
         
-        for i in nums:
-            freq[i] += 1
-            if freq[i] > len(nums)/3:
-                output.add(i)
-        return list(output)
+        for i, val in freq.items():
+            if val > threshold:
+                output.append(i)
+        return output
