a9d7f2b6a37ff33bb32f043be2aaf4c431ec562d
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-11-05 16:03:02 +0800

    Time: 574 ms (27.17%), Space: 30.4 MB (13.59%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -1,6 +1,9 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         
+        if k >= len(arr):
+            return max(arr)
+
         count, winner = 0, None
         queue = deque(arr)
         while count < k:
