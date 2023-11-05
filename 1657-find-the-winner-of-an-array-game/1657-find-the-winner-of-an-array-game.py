d68fa6c5d40c52899978eecea397b125d6b71507
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-11-05 15:52:51 +0800

    Time: 605 ms (22.28%), Space: 30.5 MB (13.59%) - LeetHub

---
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -0,0 +1,30 @@
+class Solution:
+    def getWinner(self, arr: List[int], k: int) -> int:
+        
+        count, winner = 0, None
+        queue = deque(arr)
+        while count < k:
+            first = queue.popleft()
+            second = queue.popleft()
+            bigger = max(first, second)
+
+            if winner == bigger:
+                count += 1
+                if count > len(arr):
+                    return winner
+            else:
+                count = 1
+
+            winner = bigger
+
+            if count == k:
+                return winner
+
+            if first > second:
+                queue.appendleft(first)
+                queue.append(second)
+            else:
+                queue.appendleft(second)
+                queue.append(first)
+
+        
