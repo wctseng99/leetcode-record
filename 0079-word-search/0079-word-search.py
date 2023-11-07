34d51b2cb6091c4b22d928fb9e7c5692eaeaa5cf
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-11-07 21:01:36 +0800

    Time: 5201 ms (73.72%), Space: 16.4 MB (10.91%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,20 +1,21 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
 
-        def dfs(i: int, j: int, n: int) -> bool:
-            if n == len(word):
+        def dfs(x, y, index):
+            if index == len(word):
                 return True
-            if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] != word[n]:
-                return False
+            elif 0 <= x < len(board) and 0 <= y < len(board[0]) and board[x][y] == word[index]:
+                temp, board[x][y] = board[x][y], "X"
+                is_find = dfs(x+1, y, index+1) or dfs(x-1, y, index+1) or dfs(x, y+1, index+1) or dfs(x, y-1, index+1)
+                board[x][y] = temp
             else:
-                temp, board[i][j] = board[i][j], -1 
-                next = dfs(i+1, j, n+1) or dfs(i-1, j, n+1) or dfs(i, j+1, n+1) or dfs(i, j-1, n+1)
-                board[i][j] = temp
-                return next
+                return False
+            return is_find
 
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(i, j, 0):
-                    return True
-        return False
+                if board[i][j] == word[0]:
+                    if dfs(i, j, 0):
+                        return True
                     
+        return False
