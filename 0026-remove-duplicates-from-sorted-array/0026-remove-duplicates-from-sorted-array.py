b3bcb47de42765b68643095f67ce8080039c79ca
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-12-10 22:19:46 +0800

    Time: 86 ms (53.57%), Space: 18.1 MB (13.97%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        appear = set()
-        index = 0
-        for val in nums:
-            if val not in appear:
-                appear.add(val)
-                nums[index] = val
-                index += 1
-        return index
+        nums_char = len(set(nums))
+        unique = set()
+        idx = 0
+        for i, char in enumerate(nums):
+            if char not in unique:
+                nums[idx] = char
+                idx += 1
+                unique.add(char)
+            elif len(unique) == nums_char:
+                nums[i] = "_"
         
+        return nums_char
