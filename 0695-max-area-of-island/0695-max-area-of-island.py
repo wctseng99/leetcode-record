69437d9578d3361f13da5681854fc61a54a51271
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-12-18 21:31:45 +0800

    Time: 137 ms (35.95%), Space: 19.1 MB (57.60%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -4,15 +4,15 @@ class Solution:
         def dfs(x, y):
             if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == 0:
                 return 0
-                
-            grid[x][y] = 0
-            return 1 + dfs(x+1, y) + dfs(x-1, y) + dfs(x, y+1) + dfs(x, y-1)
 
+            grid[x][y] = 0
+            return 1 + dfs(x+1, y) + dfs(x-1, y) + dfs(x, y+1) + dfs(x, y-1) 
+        
         max_area = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    current_area = dfs(i, j)
-                    max_area = max(max_area, current_area)
-
+                    curr_area = dfs(i, j)
+                    max_area = max(curr_area, max_area)
+        
         return max_area
