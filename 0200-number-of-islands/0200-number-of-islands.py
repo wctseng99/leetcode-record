3624f6ac7c3f0e8cb01f2f5729ef1997a33c2d46
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-12-20 16:03:15 +0800

    Time: 289 ms (59.15%), Space: 19 MB (60.23%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,20 +1,17 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        queue = deque([])
-        island = 0
+        def dfs(x, y):
+            if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]) or grid[x][y] == "0":
+                return 
+            grid[x][y] = "0"
+            for row, col in [x+1, y], [x-1, y] ,[x, y+1], [x, y-1]:
+                dfs(row, col)
+
+        islands = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == "1":
-                    grid[i][j] = 0
-                    queue.append((i, j))
-                    self.adjacent_lands(queue, grid)
-                    island += 1
-        return island
-
-    def adjacent_lands(self, queue, grid):   
-        while queue:
-            i, j = queue.popleft()
-            for row, col in (i+1, j), (i-1, j), (i, j+1), (i, j-1):         
-                if 0 <= row < len(grid) and 0 <= col < len(grid[0]) and grid[row][col] == "1":
-                    queue.append((row, col))
-                    grid[row][col] = 0
+                    islands += 1
+                    dfs(i, j)
+                    
+        return islands
