44a066ef2a39bb8cdd1eb91e011354c5629482ee
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2023-12-21 18:25:53 +0800

    Time: 116 ms (99.80%), Space: 18.9 MB (21.46%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,29 +3,29 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        visited = set()
-
         def dfs(x, y):
             if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or board[x][y] == "X" or (x, y) in visited:
                 return 
             visited.add((x, y))
-            dfs(x+1, y)
-            dfs(x-1, y)
-            dfs(x, y+1)
-            dfs(x, y-1)
-        
+            dfs(x + 1, y)
+            dfs(x - 1, y)
+            dfs(x, y + 1)
+            dfs(x, y - 1)
 
+        visited = set()
         for i in range(len(board)):
-            # check the upper and lower bound
+            # left and right
             dfs(i, 0)
-            dfs(i, len(board[0])-1)
-        
+            dfs(i, len(board[0]) - 1)
+            
         for i in range(len(board[0])):
-            # check the left and right bound
+            # upper and lower
             dfs(0, i)
-            dfs(len(board)-1, i)
+            dfs(len(board) - 1, i)
+        
 
         for i in range(len(board)):
             for j in range(len(board[0])):
                 if (i, j) not in visited and board[i][j] == "O":
                     board[i][j] = "X"
+                
