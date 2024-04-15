d82f2bf4597467e7eedd1b766abbe6ae2a669ab0
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2024-04-15 21:25:52 +0800

    Time: 31 ms (91.16%), Space: 16.6 MB (35.23%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -2,10 +2,10 @@ class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         s_dic = Counter(s)
         t_dic = Counter(t)
-
-        for char in t_dic:
-            if t_dic[char] != s_dic[char]:
-                return char
+        
+        for i in t_dic:
+            if s_dic[i] != t_dic[i]:
+                return i
         
 
             
