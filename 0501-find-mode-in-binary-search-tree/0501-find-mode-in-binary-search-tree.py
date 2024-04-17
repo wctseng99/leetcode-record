7a0fef392de35da2deb48dc38a2e3bc8b0a0f094
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2024-04-17 14:18:50 +0800

    Time: 44 ms (75.42%), Space: 19.9 MB (42.29%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -8,14 +8,13 @@ class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         if not root: return None
         freq = defaultdict(int)
-
-        # bfs
+        
         def dfs(node):
             if not node: return None
             freq[node.val] += 1
             dfs(node.left)
             dfs(node.right)
-
+        
         dfs(root)
-        max_freq = max(freq.values())
-        return [key for key, val in freq.items() if val == max_freq]
+        max_val = max(freq.values())
+        return [key for key, val in freq.items() if val == max_val] 
