b94363f007130b408f24d6012816380bb3cfbd0e
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2024-05-07 13:01:52 +0800

    Time: 520 ms (67.50%), Space: 30.2 MB (6.90%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,15 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_area = 0
-        left = 0
-        right = len(height) - 1
-        # move two pointers from outside to inside
-        while left != right:
-            curr_area = (right -left) * min(height[left], height[right])
-            max_area = max(max_area, curr_area)
-            if height[left] <= height[right]:
+        left, right = 0, len(height)-1
+        max_area = (right - left) * min(height[left], height[right])
+        
+        while left < right:
+            if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-
+            cur_area = (right - left) * min(height[left], height[right])
+            max_area = max(max_area, cur_area)
         return max_area
