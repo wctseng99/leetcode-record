dad3fe5b7f284cc11a8577a04c7585f04d597355
Jim Tseng <83196642+wctseng99@users.noreply.github.com>
2024-06-13 13:08:02 +0800

    Time: 546 ms (10.71%), Space: 29.4 MB (73.91%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         def find_area(l, r):
-            return (r - l) * min(height[l], height[r])
+            return (r - l) * min(height[right], height[left])
         
-        left, right = 0, len(height)-1
+        left, right = 0, len(height) - 1 
         max_area = find_area(left, right)
-        
         while left < right:
             if height[left] < height[right]:
                 left += 1
@@ -14,3 +13,4 @@ class Solution:
             cur_area = find_area(left, right)
             max_area = max(max_area, cur_area)
         return max_area
+            
